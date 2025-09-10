e2bd923dc94d44b7cbc5fc82553c707da91cae23
eaudunord <sellner.joe@gmail.com>
2025-09-09 23:40:33 -0500

    allow port reuse to make restarts faster

--- webserver.py
+++ webserver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#web_server_version=2025.09.06.1441
+#web_server_version=2025.09.09.2338
 
 import sys
 import subprocess
@@ -168,6 +168,7 @@ class LinkCableHandler(httpserver.SimpleHTTPRequestHandler):
 
     def handle_dreampi_start(self):
         try:
+            process_output = []
             start_dreampi = subprocess.check_output(["sudo", "service", "dreampi", "start"])
             result = subprocess.run(["systemctl", "is-active", "dreampi"], stdout=subprocess.DEVNULL)
             if result.returncode == 0:
@@ -192,6 +193,7 @@ class LinkCableHandler(httpserver.SimpleHTTPRequestHandler):
 
     def handle_dreampi_stop(self):
         try:
+            process_output = []
             stop_dreampi = subprocess.check_output(["sudo", "service", "dreampi", "stop"])
             result = subprocess.run(["systemctl", "is-active", "dreampi"], stdout=subprocess.DEVNULL)
             if result.returncode != 0:
@@ -407,8 +409,10 @@ class LinkCableHandler(httpserver.SimpleHTTPRequestHandler):
 
 
 if __name__ == "__main__":
+
     try:
         httpd = socketserver.TCPServer(("", PORT), LinkCableHandler)
+        httpd.allow_reuse_port=True
         print("DreamPi Link Cable Web Server Starting...")
         print("Access from any device: http://your-dreampi-ip:{}".format(PORT))
         print("Local access: http://localhost:{}".format(PORT))
